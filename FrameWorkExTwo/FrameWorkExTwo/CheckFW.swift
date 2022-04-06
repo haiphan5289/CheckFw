a7371dc06dcc05f801ac8b737de2469bda05ceca
haiphan5289 <haiphan5289@gmail.com>
2022-04-06 15:07:50 +0700

    framework

---
+++ FrameWorkExTwo/FrameWorkExTwo/CheckFW.swift
@@ -0,0 +1,18 @@
+//
+//  CheckFW.swift
+//  FrameWorkExTwo
+//
+//  Created by haiphan on 06/04/2022.
+//
+
+import Foundation
+
+public struct CheckFw {
+    
+//    public static var shared = CheckFw()
+    
+    static public func say() {
+        print("== Say")
+    }
+    
+}
---
+++ FrameWorkExTwo/FrameWorkExTwo/FrameWorkExTwo.h
@@ -0,0 +1,18 @@
+//
+//  FrameWorkExTwo.h
+//  FrameWorkExTwo
+//
+//  Created by haiphan on 06/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameWorkExTwo.
+FOUNDATION_EXPORT double FrameWorkExTwoVersionNumber;
+
+//! Project version string for FrameWorkExTwo.
+FOUNDATION_EXPORT const unsigned char FrameWorkExTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWorkExTwo/PublicHeader.h>
+
+

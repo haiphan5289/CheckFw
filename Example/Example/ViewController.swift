29e14901f4118398c83423fa6d9e56b2f1e22e4c
haiphan5289 <haiphan5289@gmail.com>
2022-04-06 16:26:35 +0700

    pushhhh

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by haiphan on 06/04/2022.
+//
+
+import UIKit
+import MaccoValidator
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        CheckFw.say()
+    }
+
+
+}
+
